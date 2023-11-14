e63bf931cc977210cbc981c745ee5b0d44aaff09
INOPIAE <m.maengel@inopiae.de>
2023-11-14 09:51:32 +0100

    initial commit: implemement ClsReplacement.vb

---
+++ inoPBI/TestInoPBI/Tests/TestClsReplacement.vb
@@ -0,0 +1,82 @@
+﻿Imports System.Collections.ObjectModel
+Imports System.IO
+Imports System.Runtime
+Imports inoPBIDLL
+Imports NUnit.Framework
+Imports NUnit.Framework.Internal
+
+Namespace TestInoPBI
+    Public Class TestClsReplacement
+        Private testPath As String = Path.GetDirectoryName(Path.GetDirectoryName(TestContext.CurrentContext.TestDirectory)).Replace("\bin", "")
+        'Private DBFile As String = testPath & "\TestData\DB\pfalzenCurrentDB.accdb"
+        Private cRep As ClsReplacement
+        Private cHelper As New ClsHelper
+        Private testFolder As String
+        Private DBFileTest As String
+
+        <SetUp>
+        Public Sub Setup()
+            testFolder = cHelper.CreateTestFolder("currenttestdata")
+            cHelper.DeleteTestFolder(testFolder)
+            'DBFileTest = testFolder & "\pfalzenCurrentDB.accdb"
+            'File.Copy(DBFile, DBFileTest)
+            cRep = New ClsReplacement
+        End Sub
+
+        <TearDown>
+        Public Sub TearDown()
+            cHelper.DeleteTestFolder(testFolder)
+        End Sub
+
+        <Test>
+        Public Sub TestGetReplacements()
+            Dim strFile As String = "\TestData\testDataReplacement.txt"
+            Dim cr As Collection(Of ClsReplacement.Replacement) = cRep.GetReplacements(testPath & strFile)
+
+            Assert.AreEqual(4, cr.Count)
+            Assert.AreEqual("HRworksOrdner", cr.Item(0).Title)
+        End Sub
+
+        <Test>
+        Public Sub TestReplaceReferences()
+            Dim strFile As String = testPath & "\TestData\test.bim"
+            Dim strFileOut As String = testFolder & "\testout.bim"
+            Dim strReplacement As String = testPath & "\TestData\testDataReplacement.txt"
+
+            Dim cr As Collection(Of ClsReplacement.Replacement) = cRep.GetReplacements(strReplacement)
+
+            Assert.True(cRep.ReplaceReferences(strFile, strReplacement, strFileOut))
+
+            Dim strTest As String
+            Using sr As New StreamReader(strFileOut)
+                strTest = sr.ReadToEnd
+            End Using
+
+            For testCount As Integer = 0 To cr.Count - 1
+                Assert.True(strTest.Contains(cr.Item(testCount).Title))
+                Assert.False(strTest.Contains(cr.Item(testCount).StrFrom))
+                Assert.True(strTest.Contains(cr.Item(testCount).StrTo))
+            Next
+        End Sub
+
+        <Test>
+        Public Sub TestReplaceReferencesError()
+            Dim strFile As String = testPath & "\TestData\test1.bim"
+            Dim strFileOut As String = testFolder & "\testout.bim"
+            Dim strReplacement As String = testPath & "\TestData\testDataReplacement.txt"
+
+            Assert.False(cRep.ReplaceReferences(strFile, strReplacement, strFileOut))
+
+            strFile = testPath & "\TestData\test.bim"
+            strReplacement = testPath & "\TestData\testDataReplacementE1.txt"
+
+            Assert.False(cRep.ReplaceReferences(strFile, strReplacement, strFileOut))
+
+            strFile = testPath & "\TestData\test.bim"
+            strReplacement = testPath & "\TestData\testDataReplacementE2.txt"
+
+            Assert.False(cRep.ReplaceReferences(strFile, strReplacement, strFileOut))
+
+        End Sub
+    End Class
+End Namespace
---
+++ inoPBI/inoPBIDLL/classes/ClsReplacement.vb
@@ -0,0 +1,69 @@
+﻿Imports System.Collections.ObjectModel
+Imports System.IO
+
+Public Class ClsReplacement
+
+    Public Structure Replacement
+        Dim Title As String
+        Dim StrFrom As String
+        Dim StrTo As String
+    End Structure
+    Public Function ReplaceReferences(strFile As String, strReplacement As String, strFileOut As String) As Boolean
+        Dim cr As Collection(Of ClsReplacement.Replacement) = GetReplacements(strReplacement)
+        Dim blnReplace As Boolean = False
+        Dim blnEnd As Boolean = False
+        Dim strOutput As String = ""
+        Dim crCount As Integer = 0
+
+        Using sr As New StreamReader(strFile)
+            While sr.Peek() >= 0
+                Dim strLine As String = sr.ReadLine
+
+                If strLine.Contains(cr.Item(crCount).Title) Then
+                    blnReplace = True
+                End If
+
+                If blnReplace = True Then
+                    If strLine.Contains(cr.Item(crCount).StrFrom) Then
+                        strLine = strLine.Replace(cr.Item(crCount).StrFrom, cr.Item(crCount).StrTo)
+                        crCount += 1
+                        blnReplace = False
+                        If crCount = cr.Count Then
+                            crCount = cr.Count - 1
+                            blnEnd = True
+                        End If
+                    End If
+                End If
+                strOutput &= strLine & vbCrLf
+            End While
+        End Using
+
+        If crCount <= cr.Count - 1 And blnEnd = False Then
+            Return False
+        End If
+
+        Using sw As New StreamWriter(strFileOut, False, System.Text.Encoding.Default)
+            sw.Write(strOutput)
+        End Using
+
+
+        Return True
+    End Function
+
+    Public Function GetReplacements(strFile As String) As Collection(Of Replacement)
+        Dim rc As New Collection(Of Replacement)
+
+        Using sr As New StreamReader(strFile)
+            While sr.Peek() >= 0
+                Dim strData() As String = sr.ReadLine.Split(";")
+                Dim r As Replacement
+                r.Title = strData(0).Trim
+                r.StrFrom = strData(1).Trim
+                r.StrTo = strData(2).Trim
+                rc.Add(r)
+            End While
+        End Using
+
+        Return rc
+    End Function
+End Class
