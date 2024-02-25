4293c5f0bab78b9adf8d900cca8a6ecb3cb97733
INOPIAE <m.maengel@inopiae.de>
2024-02-25 08:51:05 +0100

    add:  implement handling of measures in TMDL

---
+++ inoPBI/TestInoPBI/Tests/TestClsTMDLHandling.vb
@@ -0,0 +1,193 @@
+﻿Imports System.Collections.ObjectModel
+Imports System.ComponentModel.DataAnnotations.Schema
+Imports System.IO
+Imports System.Runtime
+Imports inoPBIDLL
+Imports Newtonsoft.Json.Linq
+Imports NUnit.Framework
+Imports NUnit.Framework.Internal
+
+Namespace TestInoPBI
+    Public Class TestClsTMDLHandling
+        Private testPath As String = Path.GetDirectoryName(Path.GetDirectoryName(TestContext.CurrentContext.TestDirectory)).Replace("\bin", "")
+        'Private DBFile As String = testPath & "\TestData\DB\pfalzenCurrentDB.accdb"
+        Private cTMDL As ClsTMDLHandling
+        Private cHelper As New ClsHelper
+        Private testFolder As String
+        Private DBFileTest As String
+
+        <SetUp>
+        Public Sub Setup()
+            testFolder = cHelper.CreateTestFolder("currenttestdata")
+            cHelper.DeleteTestFolder(testFolder)
+            'DBFileTest = testFolder & "\pfalzenCurrentDB.accdb"
+            'File.Copy(DBFile, DBFileTest)
+            cTMDL = New ClsTMDLHandling
+        End Sub
+
+        <TearDown>
+        Public Sub TearDown()
+            cHelper.DeleteTestFolder(testFolder)
+        End Sub
+
+        <Test>
+        Public Sub TestReadMeasuresFile()
+            Dim strFile As String = testPath & "\TestData\tmdl\Nordwind.Dataset\definition\tables\_Measures.tmdl"
+
+            cTMDL.Elements.Clear()
+            cTMDL.ReadTable(strFile)
+
+            Dim testElements As List(Of ClsTMDLHandling.Element) = cTMDL.Elements
+
+            Assert.That(testElements.Count, NUnit.Framework.Is.EqualTo(7))
+
+
+            Dim table As String = "_Measures"
+            Dim mName As String = "Umsatz"
+            Dim description As String = vbNullString
+            Dim value As String = "sum(Nordwind[Endpreis])"
+            Dim displayFolder As String = "Umsatz"
+            Dim eType As ClsTMDLHandling.ElementType = ClsTMDLHandling.ElementType.Measure
+            Dim pos As Long = 0
+
+            TestSingleElement(testElements, table, mName, description, value, displayFolder, eType, pos)
+
+            pos = 1
+            mName = "Umsatz Vorjahr"
+            value = "CALCULATE(_Measures[Umsatz],SAMEPERIODLASTYEAR(Datumstabelle[Datum]))"
+            TestSingleElement(testElements, table, mName, description, value, displayFolder, eType, pos)
+
+            pos = 2
+            mName = "Menge"
+            value = "SUM(Nordwind[Anzahl])"
+            displayFolder = Nothing
+            TestSingleElement(testElements, table, mName, description, value, displayFolder, eType, pos)
+
+            pos = 3
+            mName = "Tacho_Max"
+            value = "IF(_Measures[Umsatz Vorjahr]>_Measures[Umsatz],_Measures[Umsatz Vorjahr]*1.2,_Measures[Umsatz]*1.2)"
+            TestSingleElement(testElements, table, mName, description, value, displayFolder, eType, pos)
+
+
+            pos = 4
+            mName = "Tacho_Min"
+            value = "IF(_Measures[Umsatz Vorjahr]<_Measures[Umsatz],_Measures[Umsatz Vorjahr]*0.8,_Measures[Umsatz]*0.8)"
+            TestSingleElement(testElements, table, mName, description, value, displayFolder, eType, pos)
+
+            pos = 5
+            mName = "Kunden"
+            value = "DISTINCTCOUNT(Nordwind[Firma])"
+            description = "Anzahl der eindeutigen Kunden"
+            TestSingleElement(testElements, table, mName, description, value, displayFolder, eType, pos)
+
+            pos = 6
+            mName = "_Measures"
+            value = "let" & vbCrLf & "    Quelle = Table.FromRows"
+            description = vbNullString
+            eType = ClsTMDLHandling.ElementType.PowerQuery
+            TestSingleElement(testElements, table, mName, description, value, displayFolder, eType, pos, True)
+        End Sub
+
+        Private Shared Sub TestSingleElement(testElements As List(Of ClsTMDLHandling.Element), table As String, mName As String, description As String, value As String, displayFolder As String, eType As ClsTMDLHandling.ElementType, pos As Long, Optional blnSource As Boolean = False)
+            Assert.That(testElements(pos).TableName, NUnit.Framework.Is.EqualTo(table))
+            Assert.That(testElements(pos).Name, NUnit.Framework.Is.EqualTo(mName))
+            Assert.That(testElements(pos).Description, NUnit.Framework.Is.EqualTo(description))
+            If blnSource = True Then
+                Assert.That(testElements(pos).Value.Substring(0, value.Length), NUnit.Framework.Is.EqualTo(value))
+            Else
+                Assert.That(testElements(pos).Value, NUnit.Framework.Is.EqualTo(value))
+            End If
+            Assert.That(testElements(pos).Displayfolder, NUnit.Framework.Is.EqualTo(displayFolder))
+            Assert.That(testElements(pos).Type, NUnit.Framework.Is.EqualTo(eType))
+        End Sub
+
+        <Test>
+        Public Sub TestReadNeukundenFile()
+            Dim strFile As String = testPath & "\TestData\tmdl\Nordwind.Dataset\definition\tables\Neukunden.tmdl"
+
+            cTMDL.Elements.Clear()
+            cTMDL.ReadTable(strFile)
+
+            Dim testElements As List(Of ClsTMDLHandling.Element) = cTMDL.Elements
+
+            Assert.That(testElements.Count, NUnit.Framework.Is.EqualTo(1))
+
+
+            Dim table As String = "Neukunden"
+            Dim mName As String = "Neukunden"
+            Dim description As String = vbNullString
+            Dim value As String = "SUMMARIZE(" & vbCrLf & "    Nordwind,"
+            Dim displayFolder As String = Nothing
+            Dim eType As ClsTMDLHandling.ElementType = ClsTMDLHandling.ElementType.PowerQuery
+            Dim pos As Long = 0
+
+            TestSingleElement(testElements, table, mName, description, value, displayFolder, eType, pos, True)
+        End Sub
+
+
+        <Test>
+        Public Sub TestReadNordwindFile()
+            Dim strFile As String = testPath & "\TestData\tmdl\Nordwind.Dataset\definition\tables\Nordwind.tmdl"
+
+            cTMDL.Elements.Clear()
+            cTMDL.ReadTable(strFile)
+
+            Dim testElements As List(Of ClsTMDLHandling.Element) = cTMDL.Elements
+
+            Assert.That(testElements.Count, NUnit.Framework.Is.EqualTo(2))
+
+
+            Dim table As String = "Nordwind"
+            Dim mName As String = "Region"
+            Dim description As String = vbNullString
+            Dim value As String = "RELATED(Regionen[Region])"
+            Dim displayFolder As String = Nothing
+            Dim eType As ClsTMDLHandling.ElementType = ClsTMDLHandling.ElementType.Column
+            Dim pos As Long = 0
+
+            TestSingleElement(testElements, table, mName, description, value, displayFolder, eType, pos)
+
+            pos = 1
+            mName = "Nordwind"
+            value = "let" & vbCrLf & "    Quelle = Csv.Document(File.Contents("
+            description = vbNullString
+            eType = ClsTMDLHandling.ElementType.PowerQuery
+            TestSingleElement(testElements, table, mName, description, value, displayFolder, eType, pos, True)
+        End Sub
+
+        <Test>
+        Public Sub TestReadTables()
+            Dim strFile As String = testPath & "\TestData\tmdl\Nordwind.Dataset\definition\tables"
+
+            Dim testElements As List(Of ClsTMDLHandling.Element) = cTMDL.Elements
+
+            Assert.That(testElements.Count, NUnit.Framework.Is.EqualTo(0))
+
+            cTMDL.ReadTables(strFile)
+
+            testElements = cTMDL.Elements
+
+            Assert.That(testElements.Count, NUnit.Framework.Is.EqualTo(12))
+
+            Dim tables As New List(Of String)()
+            For Each el As ClsTMDLHandling.Element In testElements
+                Dim strTable As String = el.TableName
+                If tables.FirstOrDefault(Function(x) x = strTable) <> strTable Then
+                    tables.Add(strTable)
+                End If
+            Next
+
+            Assert.That(tables.Count, NUnit.Framework.Is.EqualTo(5))
+            Assert.That(tables, Has.Member("_Measures"))
+            Assert.That(tables, Has.Member("Datumstabelle"))
+            Assert.That(tables, Has.Member("Neukunden"))
+            Assert.That(tables, Has.Member("Nordwind"))
+            Assert.That(tables, Has.Member("Regionen"))
+
+            Assert.That(tables, Has.No.Member("DateTableTemplate_hash"))
+            Assert.That(tables, Has.No.Member("LocalDateTable_hash"))
+
+
+        End Sub
+    End Class
+End Namespace
---
+++ inoPBI/inoPBIDLL/classes/ClsTMDLHandling.vb
@@ -0,0 +1,120 @@
+﻿Imports System.Diagnostics.Metrics
+Imports System.IO
+Imports System.Net.Mime.MediaTypeNames
+Imports System.Reflection.Metadata
+Imports System.Text
+Imports inoPBIDLL.ClsJSONHandling
+
+Public Class ClsTMDLHandling
+
+    Public Structure Element
+        Public Name As String
+        Public Value As String
+        Public Displayfolder As String
+        Public Description
+        Public Type As ElementType
+        Public TableName As String
+    End Structure
+
+    Public Enum ElementType
+        NotDefined
+        Measure
+        PowerQuery
+        Column
+    End Enum
+
+    Public Elements As New List(Of Element)
+
+    Public Sub ReadTables(strPath As String)
+        Elements.Clear()
+        For Each f As String In Directory.GetFiles(strPath)
+            If f.Contains("\LocalDateTable") Then Continue For
+            If f.Contains("\DateTableTemplate") Then Continue For
+            ReadTable(f)
+        Next
+    End Sub
+
+    Public Sub ReadTable(f As String)
+
+        Dim reader As New StreamReader(f, Encoding.UTF8)
+        Dim table As String = Path.GetFileNameWithoutExtension(f)
+        Dim description As String = vbNullString
+        Dim myElement As New Element
+        Dim blnNew As Boolean = False
+        Dim blnSource As Boolean = False
+        Dim delimiter As String = vbNullString
+
+        Do Until reader.Peek = -1
+            Dim lineplain As String = reader.ReadLine
+            Dim line As String = lineplain.Trim
+            If line.Length > 0 Then
+                If line.StartsWith("///") Then
+                    description &= line.Substring(4)
+                    Continue Do
+                End If
+                Dim start() As String = line.Split(" ")
+                Select Case start(0)
+                    Case "measure"
+                        Dim m() As String = line.Substring(7).Split("=")
+                        If blnNew = True Then
+                            Elements.Add(myElement)
+                        End If
+                        myElement = New Element
+                        myElement.TableName = table
+                        myElement.Description = description
+                        myElement.Name = m(0).Trim.Replace("'", vbNullString)
+                        myElement.Value = m(1).Trim
+                        myElement.Type = ElementType.Measure
+                        description = vbNullString
+                        blnNew = True
+                    Case "displayFolder:"
+                        myElement.Displayfolder = line.Substring(14).Trim
+                    Case "annotation"
+                        If blnNew = True Then
+                            Elements.Add(myElement)
+                            blnNew = False
+                            blnSource = False
+                            delimiter = vbNullString
+                        End If
+                    Case "partition"
+                        If blnNew = True Then
+                            Elements.Add(myElement)
+                        End If
+                        myElement = New Element
+                        myElement.TableName = table
+                        myElement.Description = description
+                        myElement.Name = table
+                        myElement.Type = ElementType.PowerQuery
+                        description = vbNullString
+                        blnNew = True
+                    Case "source"
+                        blnSource = True
+                    Case "column"
+                        If line.Contains("=") Then
+                            Dim m() As String = line.Substring(6).Split("=")
+                            If blnNew = True Then
+                                Elements.Add(myElement)
+                            End If
+                            myElement = New Element
+                            myElement.TableName = table
+                            myElement.Description = description
+                            myElement.Name = m(0).Trim.Replace("'", vbNullString)
+                            myElement.Value = m(1).Trim
+                            myElement.Type = ElementType.Column
+                            description = vbNullString
+                            Elements.Add(myElement)
+                            blnNew = False
+                        End If
+                    Case Else
+                        If blnSource = True Then
+                            myElement.Value &= delimiter & lineplain.Replace(vbTab, vbNullString)
+                            delimiter = vbCrLf
+                        End If
+                End Select
+            End If
+        Loop
+        If blnNew = True Then
+            Elements.Add(myElement)
+        End If
+    End Sub
+End Class
